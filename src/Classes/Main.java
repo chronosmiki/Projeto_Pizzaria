d22bb70169163991f744d85351d42b034e6571b7
Vitor Pereira <vitorj0829@hotmail.com>
2016-08-30 19:24:24 -0300

    Add files via upload

---
+++ src/Classes/Main.java
@@ -0,0 +1,18 @@
+package Classes;
+
+
+import Views.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Main {
+    
+    public static void main(String[] args){
+        
+        SplashScreem splash = new SplashScreem();
+        splash.mostraSplash();  
+              
+   
+    }    
+}
