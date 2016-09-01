ace02010fd2742a77949120a696f184e758c98dc
ColluraDeveloper <roberto.collura@etec.sp.gov.br>
2016-09-01 20:37:21 -0300

    Criacao dos pacotes MVC

---
+++ src/Main/Main.java
@@ -0,0 +1,19 @@
+package Main;
+
+
+import ClassesAuxiliares.SplashScreem;
+import Views.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Main {
+    
+    public static void main(String[] args){
+        
+        SplashScreem splash = new SplashScreem();
+        splash.mostraSplash();  
+              
+   
+    }    
+}
