4ef75ce5d0ea775470469777e97576eac9a285fc
ColluraDeveloper <roberto.collura@etec.sp.gov.br>
2016-09-01 21:15:08 -0300

    Criacao Classes Models

--- src/Models/ClienteModel.java
+++ src/Models/ClienteModel.java
@@ -11,6 +11,63 @@ package Models;
  */
 public class ClienteModel {
     
+    private int idCliente;
+    private String nome;
+    private String sobrenome;
+    private int idTelefone;
+    private String dataCadastro;
+    private String email;
+    
+    
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdTelefone() {
+        return idTelefone;
+    }
+
+    public void setIdTelefone(int idTelefone) {
+        this.idTelefone = idTelefone;
+    }
+
+    public String getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(String dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     
     
 }
---
+++ src/Models/UsuarioModel.java
@@ -0,0 +1,63 @@
+
+package Models;
+
+public class UsuarioModel {
+    
+    private int idUsuario;
+    private String nome;
+    private String sobrenome;
+    private String login;
+    private String password;
+    private int nivel;
+    
+    
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+    
+}
