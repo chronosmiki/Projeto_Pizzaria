daadb44684c20bcd176aa598111f794d0c11de5b
Vitor Pereira <vitorj0829@hotmail.com>
2016-09-10 14:50:11 -0300

    CRUD ClientesDao
    Inicio do crud, metodo de inserir pronto!

--- src/Dao/ClienteDao.java
+++ src/Dao/ClienteDao.java
@@ -1,19 +1,44 @@
 
 package Dao;
 
+import ClassesAuxiliares.Msg;
+import static Dao.ConexaoMysql.open;
 import Interfaces.interfaceDao;
 import Models.ClienteModel;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ClienteDao implements interfaceDao{
     ClienteModel clienteModel;
-    ArrayList clientes;
+    ResultSet resultado = null;   
+    String sql = "";          
+    Statement stmt;
+    PreparedStatement pstm;
+    Boolean success = false;    
+    ArrayList <ClienteModel> clientes = new ArrayList();
     
 
     @Override
     public Boolean inserir(Object object) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        clienteModel = (ClienteModel) object;
+        
+        try{
+             
+            stmt = open().createStatement();
+            
+        }catch(Exception ex)
+        {
+            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+            Msg.exclamation(Msg.erroConexao + ex.getMessage());
+        }
+        
+        return success;
     }
 
     @Override
